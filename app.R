eea9ae14f5be52000ef3eef599b1aad68b4baf3f
aseematt <aseematter@kumc.edu>
2024-11-10 05:23:35 -0600

    Add files via upload

--- app.R
+++ app.R
@@ -1,30 +1,34 @@
-#
-
-library(shiny)
-
-
-data <- read.csv("C:\\Users\\WHS_c\\Downloads\\824-Data Visualization\\KansasPopulation.csv")
-
+#Final Project- Kansas Population Data
+#Read in libraries
+install.packages("shiny")
 library(shiny)
 library(ggplot2)
 library(maps)
 library(dplyr)
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Define UI for application
 ui <- fluidPage(
   titlePanel("Kansas Population Data"),
+  
   sidebarLayout(
     sidebarPanel(
+      fileInput("file1", "Choose CSV File",
+                accept = c("text/csv", 
+                           "text/comma-separated-values,text/plain", 
+                           ".csv")),
+      
       selectInput("population_range", "Select Population Range:",
                   choices = c("Less than 10,000" = "<10000",
                               "10,001 to 50,000" = "10001-50000",
                               "50,001 to 90,000" = "50001-90000",
                               "Greater than 90,001" = ">90001")),
+      
       uiOutput("county_ui"),
       helpText("You can select up to 3 counties."),
       actionButton("update", "Update Plot")
     ),
+    
     mainPanel(
       plotOutput("plot"),
       plotOutput("map")
@@ -33,9 +37,21 @@ ui <- fluidPage(
 )
 
 # Define server logic
-server <- function(input, output, session) {
+server <- function(input, output) { 
+  # Read the file
+  output$contents <- renderTable({
+    req(input$file1)
+    inFile <- input$file1
+    if (is.null(inFile))
+      return(NULL)
+    data <- read.csv(inFile$datapath)
+    data  # Display the data
+  })
+  
+  # Filtered data based on population range
   filtered_data <- reactive({
-    req(input$population_range)
+    req(input$file1)
+    data <- read.csv(input$file1$datapath)
     if (input$population_range == "<10000") {
       subset(data, Population < 10000)
     } else if (input$population_range == "10001-50000") {
@@ -47,7 +63,9 @@ server <- function(input, output, session) {
     }
   })
   
+  # UI for selecting counties
   output$county_ui <- renderUI({
+    req(filtered_data())
     selectInput("counties", "Select Counties:", 
                 choices = unique(filtered_data()$County), 
                 selected = unique(filtered_data()$County)[1], 
@@ -55,16 +73,18 @@ server <- function(input, output, session) {
                 selectize = TRUE)
   })
   
+  # Selected data based on counties
   selected_data <- reactive({
     req(input$counties)
     validate(
       need(length(input$counties) <= 3, "Please select up to 3 counties.")
     )
-    subset(data, County %in% input$counties)
+    subset(filtered_data(), County %in% input$counties)
   })
   
+  # Plot for population data
   output$plot <- renderPlot({
-    input$update  # Trigger reactivity on button click
+    input$update  # Trigger re-activity on button click
     isolate({
       ggplot(selected_data(), aes(x = Year, y = Population, color = County)) +
         geom_line() +
@@ -75,6 +95,7 @@ server <- function(input, output, session) {
     })
   })
   
+  # Map for selected counties
   output$map <- renderPlot({
     input$update  # Trigger reactivity on button click
     isolate({
@@ -90,5 +111,7 @@ server <- function(input, output, session) {
     })
   })
 }
+
 # Run the application 
 shinyApp(ui = ui, server = server)
+
